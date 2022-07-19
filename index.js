001a158f6085bdd04e97624d74b41f3190a4bdda
nasehbadalov <nasehbadalov@gmail.com>
2022-07-19 23:01:14 +0400

    asdasd

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log('LOG LOG LOG ! ! !');
-console.log("\x1b[34m", "WORKING ON VALIDITITY OF JSON...");
+const logColor = "\x1b[34m";
+
+console.log(logColor, "WORKING ON VALIDITITY OF JSON...");
+
 process.exit(0);
