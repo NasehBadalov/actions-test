0493578e028ecc93c32e5d98049ab9b60da190ed
nasehbadalov <nasehbadalov@gmail.com>
2022-07-20 03:04:30 +0400

    asd

---
+++ workflow_scripts/urlValidation.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const {getJsonFileNames} = require("./helpers/getJsonFileNames");
+const {isValidJSON} = require("./helpers/isValidJSON");
+const {log} = require("./helpers/log");
+
+const init = async () => {
+    const fileNames = await getJsonFileNames();
+
+    for (let fn of fileNames) {
+        const dirPath = path.join(__dirname, '..', `/${fn}`);
+        const rawData = fs.readFileSync(dirPath);
+
+        console.log(JSON.parse(rawData));
+    }
+}
+
+init();
