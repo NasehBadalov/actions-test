afb0c2d0b561e8e169878b8bda168ce40b8b7d61
nasehbadalov <nasehbadalov@gmail.com>
2022-07-20 00:49:40 +0400

    Asfd

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const logColor = "\x1b[34m";
+export const log = (value) => console.log("\x1b[34m", value);
 
-console.log(logColor, "WORKING ON VALIDITITY OF JSON...");
+export const isValidJSON = (str) => {
+    try{
+        JSON.parse(str);
+    } catch (e) {
+        // Has syntax error so we log a message and exit with an error code
+        console.error("JSON IS NOT VALID!");
+        process.exit(1);
+    }
+    console.log("ALL GOOD WITH THE JSON FILE!");
 
-process.exit(0);
+    process.exit(0);
+}
+
+
+// process.exit(0);
+
+const fs = require('fs');
+
+let rawData = fs.readFileSync('./test.json');
+isValidJSON(rawData);
